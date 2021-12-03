24b2a92e6506a77a3ddcc92da0b8efc2e1249a2a
Jan Vlcek <vlki@vlki.cz>
2021-12-03 08:45:31 +0100

    assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   reactStrictMode: true,
-}
+
+  // Needed for running on GitHub Pages
+  assetPrefix: isProd ? "/lpc-mapa-demo/" : "",
+};
