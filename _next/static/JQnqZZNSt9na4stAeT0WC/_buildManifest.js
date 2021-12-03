07074d5b7d757ff6cf428cbd2644737aef0aa864
vlki <vlki@vlki.cz>
2021-12-03 08:27:03 +0000

    Deploying to gh-pages from @ vlki/lpc-mapa-demo@cf3949713f3577858b6bf58b8d4302a394455665 🚀

---
+++ _next/static/JQnqZZNSt9na4stAeT0WC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/97622bc77bea8578.css","static/chunks/pages/index-7b051e0981db07e7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
