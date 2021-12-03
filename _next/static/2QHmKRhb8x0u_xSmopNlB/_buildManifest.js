2a54628a4aead9c57a48f6d4b6f381f4dd090cec
vlki <vlki@vlki.cz>
2021-12-03 09:05:34 +0000

    Deploying to gh-pages from @ vlki/lpc-mapa-demo@099057860d498d1e794bad08d45561be20c16ed7 🚀

---
+++ _next/static/2QHmKRhb8x0u_xSmopNlB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/62ce1a46e895e7b7.css","static/chunks/pages/index-be94ca87b3d97f6c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/JQnqZZNSt9na4stAeT0WC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/97622bc77bea8578.css","static/chunks/pages/index-7b051e0981db07e7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
